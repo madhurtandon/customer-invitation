085746a4d30cec2dec280f0ded1854fd7dfadb9c
Madhur Tandon <madhur.tandon@kayako.com>
2018-04-25 19:02:22 +0530

    1.) Load the classes using the composer 2.) Added the user class to get the listing of the user in a structured format 3.) Added a customer.json file in a data folder

--- Application.php
+++ Application.php
@@ -9,20 +9,36 @@
 namespace App;
 
 
+use App\Library\User;
+
+
 /**
  * A application class responsible to initialize the application
  *
- * @author Madhur Tandon
+ * @package App
  */
 class Application
 {
 	/**
-	 * This module get the customer list from the file handler and print the invitees
+	 * This method retrieve retrieve the formatted list of customer and print the invitees which are with in 100KM
 	 *
 	 * @author Madhur Tandon
 	 */
 	public function Initialize()
 	{
+		$customerData = $this->GetDataFromFile();
+		$users        = (new User())->GetFormattedList($customerData);
+	}
 
+	/**
+	 * @author Madhur Tandon
+	 *
+	 * @return array
+	 */
+	public function GetDataFromFile()
+	{
+		$file = './data' . DIRECTORY_SEPARATOR . 'customer.json';
+
+		return json_decode(file_get_contents($file), JSON_OBJECT_AS_ARRAY);
 	}
 }
--- bootstrap.php
+++ bootstrap.php
@@ -8,6 +8,9 @@
 
 define('DIR_PREFIX', dirname(__FILE__));
 
+// Autoload the Libraries that are used or supported by App
+require_once(DIR_PREFIX . '/vendor/autoload.php');
+
 require_once DIR_PREFIX . DIRECTORY_SEPARATOR . 'Application.php';
 
 (new \App\Application())->Initialize();
