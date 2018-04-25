bf78f3d11a09c74db921155616d11914b0800ba9
Madhur Tandon <madhur.tandon@kayako.com>
2018-04-25 23:16:38 +0530

    Added a user validation

---
+++ __src/library/Exception/InvalidData.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Customer Invitation
+ *
+ * @package        App
+ * @author         Madhur Tandon
+ */
+
+
+namespace App\Library;
+
+
+/**
+ * This exception is used to denote that invalid data was provided as part of arguments or result.
+ *
+ * @package App\Library
+ */
+class InvalidData extends Exception
+{
+
+}
--- __src/library/User.php
+++ __src/library/User.php
@@ -27,16 +27,44 @@ class User
 	 * @param array $data
 	 *
 	 * @return array
+	 * @throws InvalidArgument
+	 * @throws InvalidData
 	 */
 	public function GetUsers(array $data)
 	{
+		if (empty($data)) {
+			throw new InvalidArgument('Empty data provided');
+		}
+
 		$list = [];
 		foreach ($data as $record) {
+			$this->ValidateUser($record);
+
 			$list[$record[self::USER_ID]] = [self::NAME      => $record[self::NAME],
 											 self::LATITUDE  => $record[self::LATITUDE],
 											 self::LONGITUDE => $record[self::LONGITUDE]];
 		}
 
 		return $list;
+	}
+
+	/**
+	 * @author Madhur Tandon
+	 *
+	 * @param array $record
+	 *
+	 * @throws InvalidData
+	 */
+	public function ValidateUser(array $record)
+	{
+		if (!isset($record[self::USER_ID])) {
+			throw new InvalidData('User ID is missing');
+		} else if (!isset($record[self::NAME])) {
+			throw new InvalidData('User Name is missing');
+		} else if (!isset($record[self::LATITUDE])) {
+			throw new InvalidData('User Latitude is missing');
+		} else if (!isset($record[self::LONGITUDE])) {
+			throw new InvalidData('User Longitude is missing');
+		}
 	}
 }
